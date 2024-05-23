5f946a9af437a653b3974efeb69af8f5e41bdfa7
노성호 <35386626+nomore12@users.noreply.github.com>
2024-05-23 04:46:29 +0000

    랜덤 재생 완료

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,24 @@
 import React, { useState, useRef, useEffect } from "react";
 import Chords from "../components/Chords";
 import chordsData from "../data/chords.json";
+import * as Tone from "tone";
+
+const allChords = [
+  chordsData.A,
+  chordsData.Am,
+  chordsData.C,
+  chordsData.D,
+  chordsData.Dm,
+  chordsData.E,
+  chordsData.Em,
+  chordsData.F,
+  chordsData.G,
+];
+
+function getRandomElement(arr: any[]) {
+  var index = Math.floor(Math.random() * arr.length);
+  return arr[index];
+}
 
 const Main: React.FC = () => {
   const [currentChord, setCurrentChord] = useState<{
@@ -8,6 +26,65 @@ const Main: React.FC = () => {
     fingers: number[][];
     mute: number[];
   }>(chordsData.F);
+  const [player, setPlayer] = useState<Tone.Player | null>(null);
+  const [beatLoop, setBeatLoop] = useState<Tone.Loop | null>(null);
+  const [isSoundLoaded, setIsSoundLoaded] = useState(false);
+
+  const beatCount = useRef(0);
+
+  const handlePlay = async () => {
+    const playerToUse = player as Tone.Player;
+
+    if (playerToUse) {
+      await Tone.start();
+      beatCount.current = 0; // Reset beat count
+
+      const transport = Tone.getTransport();
+      transport.bpm.value = 80; // set BPM to 80
+      transport.start(); // start Transport
+
+      const loop = new Tone.Loop((time) => {
+        playerToUse.start(time);
+        beatCount.current++;
+        if ((beatCount.current - 1) % 4 === 1) {
+          console.log("show next chord", beatCount.current);
+          setCurrentChord(getRandomElement(allChords));
+        }
+      }, "4n").start(0);
+
+      setBeatLoop(loop);
+    }
+  };
+
+  const handleStop = () => {
+    // Stop the Loop
+    if (beatLoop) {
+      beatLoop.stop();
+    }
+
+    // Stop the Transport and the Player
+    Tone.getTransport().stop();
+    player?.stop();
+  };
+
+  useEffect(() => {
+    const createPlayer = async () => {
+      const url = "sounds/Kick.wav";
+      const newPlayer = new Tone.Player();
+
+      try {
+        await newPlayer.load(url);
+        setIsSoundLoaded(true);
+        newPlayer.toDestination();
+
+        setPlayer(newPlayer);
+      } catch (e) {
+        console.error("Unable to load sound file:", e);
+      }
+    };
+
+    createPlayer();
+  }, []);
 
   return (
     <div>
@@ -25,6 +102,12 @@ const Main: React.FC = () => {
         <button onClick={() => setCurrentChord(chordsData.F)}>F</button>
         <button onClick={() => setCurrentChord(chordsData.G)}>G</button>
       </div>
+      {isSoundLoaded && (
+        <>
+          <button onClick={handlePlay}>kick</button>
+          <button onClick={handleStop}>stop</button>
+        </>
+      )}
     </div>
   );
 };
