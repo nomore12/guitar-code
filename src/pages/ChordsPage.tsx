ab3e878e653d6f8d79ef4c9b886f808cd65c69e0
노성호 <nightwing@naver.com>
2024-05-30 13:40:58 +0900

    asdfasdfsaf

--- src/pages/ChordsPage.tsx
+++ src/pages/ChordsPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Flex, Text } from '@radix-ui/themes';
+import { Box, Flex, Radio, Text } from '@radix-ui/themes';
 import Chords from '../components/Chords';
 import chordsData from '../data/openChords.json';
 import * as Tone from 'tone';
@@ -20,6 +20,25 @@ const ChordsPage = () => {
             <Text>다음 코드</Text>
             <Chords chord={chordsData.X} />
           </Box>
+        </Flex>
+      </Box>
+      <Box>
+        <Text size="5" m={10}>
+          연습 방식
+        </Text>
+        <Flex gap="5" justify="center">
+          <Text>
+            <Radio name="mode" value="1" defaultChecked />
+            다이아토닉
+          </Text>
+          <Text>
+            <Radio name="mode" value="2" />
+            직접 입력
+          </Text>
+          <Text>
+            <Radio name="mode" value="3" />
+            랜덤
+          </Text>
         </Flex>
       </Box>
     </Flex>
